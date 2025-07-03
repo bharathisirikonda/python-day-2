ccf5524e08707c4476099341ea04b28c7ee67ce3
bharathisirikonda <istsbharathi@gmail.com>
2025-07-03 10:23:24 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,15 @@
+"""
+
+if
+The pythin if block is executed wken the condition is true
+syntax :
+if condition :
+statements
+
+"""
+
+#program
+a = 10
+if a==10:
+    print("the if statements is executed...")
+    
---
+++ membership operators.py
@@ -0,0 +1,54 @@
+""""
+membership operators are used to check the values in a sequence
+and returns the boolean values
+they operators are "in", not in
+"""
+x=["apple","banana"]
+print("apple" in x)
+print("pp" not in x)
+print("banana" not in x)
+print("dragon" in x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
